e946b1cea0b26bb9c220063592786ce0988942e3
AntreasK24 <AntreasKourris@gmail.com>
2025-12-01 10:01:02 +0100

    Completed day 1

---
+++ day1/day1.py
@@ -0,0 +1,37 @@
+import numpy as np
+import re
+
+
+def spltInput(line):
+    output = re.split('(\d+)', line)
+    return output
+    
+head = 50
+
+number_of_zeros = 0
+
+with open("input.txt") as file:
+    for line in file:
+        output = spltInput(line)
+        direction = output[0]
+        steps = int(output[1])
+
+        if(direction == "L"):
+            head = head + steps
+            while(head > 99):
+                head = head - 100
+        elif(direction == "R"):
+            head = head - steps
+            while(head < 0):
+                head = head + 100
+
+        print(head)
+
+        if(head == 0):
+            number_of_zeros += 1
+
+
+print("Number of zeros: ",number_of_zeros)
+
+
+
